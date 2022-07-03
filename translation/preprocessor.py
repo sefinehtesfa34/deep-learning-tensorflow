b5c963a60f56c9295a0a15f97b7b679f3f1b85e1
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-07-03 10:51:31 +0300

    load the dataset

--- translation/preprocessor.py
+++ translation/preprocessor.py
@@ -17,3 +17,13 @@ path_to_zip = tf.keras.utils.get_file(
     'spa-eng.zip', origin=url,
     extract=True)
 path_to_file = pathlib.Path(path_to_zip).parent/'spa-eng/spa.txt'
+def load_data(path):
+  text = path.read_text(encoding='utf-8')
+
+  lines = text.splitlines()
+  pairs = [line.split('\t') for line in lines]
+
+  inp = [inp for targ, inp in pairs]
+  targ = [targ for targ, inp in pairs]
+
+  return targ, inp
