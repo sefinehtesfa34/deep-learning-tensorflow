803341a1701d4db9619459f7a88bbaeae4f76da3
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-07-03 11:57:57 +0300

    vinput text vectorization and traget text vectorization separately

--- translation/text_vectorizer.py
+++ translation/text_vectorizer.py
@@ -1,8 +1,17 @@
 import tensorflow as tf
-from preprocessor import tf_lower_and_split_punct
+from preprocessor import tf_lower_and_split_punct,inp,targ
 max_vocab_size = 5000
 
 input_text_processor = tf.keras.layers.TextVectorization(
     standardize=tf_lower_and_split_punct,
     max_tokens=max_vocab_size)
+
+input_text_processor.adapt(inp)
+
+output_text_processor = tf.keras.layers.TextVectorization(
+    standardize=tf_lower_and_split_punct,
+    max_tokens=max_vocab_size)
+
+output_text_processor.adapt(targ)
+
 
