bd520acd03a1607c006f5dc00b534310c3c79122
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-07-03 13:55:05 +0300

    the call method for the decoder class

--- translation/decoder.py
+++ translation/decoder.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
-
+import typing
+from typing import Any,Tuple 
 from translation.encoder import BahdanauAttention
+from shape_checker import ShapeChecker
 # Here is the `Decoder` class and its initializer. 
 # The initializer creates all the necessary layers.
 class Decoder(tf.keras.layers.Layer):
@@ -32,3 +34,52 @@ class Decoder(tf.keras.layers.Layer):
     self.fc = tf.keras.layers.Dense(self.output_vocab_size)
 # The `call` method for this layer takes and returns multiple tensors. 
 # Organize those into simple container classes:
+class DecoderInput(typing.NamedTuple):
+  new_tokens: Any
+  enc_output: Any
+  mask: Any
+
+class DecoderOutput(typing.NamedTuple):
+  logits: Any
+  attention_weights: Any
+def call(self,
+         inputs: DecoderInput,
+         state=None) -> Tuple[DecoderOutput, tf.Tensor]:
+  shape_checker = ShapeChecker()
+  shape_checker(inputs.new_tokens, ('batch', 't'))
+  shape_checker(inputs.enc_output, ('batch', 's', 'enc_units'))
+  shape_checker(inputs.mask, ('batch', 's'))
+
+  if state is not None:
+    shape_checker(state, ('batch', 'dec_units'))
+
+  # Step 1. Lookup the embeddings
+  vectors = self.embedding(inputs.new_tokens)
+  shape_checker(vectors, ('batch', 't', 'embedding_dim'))
+
+  # Step 2. Process one step with the RNN
+  rnn_output, state = self.gru(vectors, initial_state=state)
+
+  shape_checker(rnn_output, ('batch', 't', 'dec_units'))
+  shape_checker(state, ('batch', 'dec_units'))
+
+  # Step 3. Use the RNN output as the query for the attention over the
+  # encoder output.
+  context_vector, attention_weights = self.attention(
+      query=rnn_output, value=inputs.enc_output, mask=inputs.mask)
+  shape_checker(context_vector, ('batch', 't', 'dec_units'))
+  shape_checker(attention_weights, ('batch', 't', 's'))
+
+  # Step 4. Eqn. (3): Join the context_vector and rnn_output
+  #     [ct; ht] shape: (batch t, value_units + query_units)
+  context_and_rnn_output = tf.concat([context_vector, rnn_output], axis=-1)
+
+  # Step 4. Eqn. (3): `at = tanh(Wc@[ct; ht])`
+  attention_vector = self.Wc(context_and_rnn_output)
+  shape_checker(attention_vector, ('batch', 't', 'dec_units'))
+
+  # Step 5. Generate logit predictions:
+  logits = self.fc(attention_vector)
+  shape_checker(logits, ('batch', 't', 'output_vocab_size'))
+
+  return DecoderOutput(logits, attention_weights), state
