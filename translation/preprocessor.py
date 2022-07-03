76ef73820999baf10f0979e73ddfd498a955e4c0
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-07-03 10:50:37 +0300

    download the dataset

--- translation/preprocessor.py
+++ translation/preprocessor.py
@@ -9,3 +9,11 @@ import tensorflow_text as tf_text
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import pathlib
+
+# Download the file
+url="http://www.manythings.org/anki/spa-eng.zip"
+path_to_zip = tf.keras.utils.get_file(
+    'spa-eng.zip', origin=url,
+    extract=True)
+path_to_file = pathlib.Path(path_to_zip).parent/'spa-eng/spa.txt'
