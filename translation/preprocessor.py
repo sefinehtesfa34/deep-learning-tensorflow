84842df7c78541c2b6a902e7e92d3fb3e9b95187
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-07-03 11:52:53 +0300

    added text standardized function

--- translation/preprocessor.py
+++ translation/preprocessor.py
@@ -49,3 +49,16 @@ dataset = dataset.batch(BATCH_SIZE)
 # The tensorflow_text package contains a unicode normalize operation:
 
 # Unicode normalization will be the first step in the text standardization function:
+def tf_lower_and_split_punct(text):
+  # Split accecented characters.
+  text = tf_text.normalize_utf8(text, 'NFKD')
+  text = tf.strings.lower(text)
+  # Keep space, a to z, and select punctuation.
+  text = tf.strings.regex_replace(text, '[^ a-z.?!,¿]', '')
+  # Add spaces around punctuation.
+  text = tf.strings.regex_replace(text, '[.?!,¿]', r' \0 ')
+  # Strip whitespace.
+  text = tf.strings.strip(text)
+
+  text = tf.strings.join(['[START]', text, '[END]'], separator=' ')
+  return text
