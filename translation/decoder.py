4118b9345cb782cf29348738abb22809eb426c5b
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-07-03 13:48:30 +0300

    decoder layer is added

---
+++ translation/decoder.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+from translation.encoder import BahdanauAttention
+# Here is the `Decoder` class and its initializer. 
+# The initializer creates all the necessary layers.
+class Decoder(tf.keras.layers.Layer):
+  def __init__(self, output_vocab_size, embedding_dim, dec_units):
+    super(Decoder, self).__init__()
+    self.dec_units = dec_units
+    self.output_vocab_size = output_vocab_size
+    self.embedding_dim = embedding_dim
+
+    # For Step 1. The embedding layer convets token IDs to vectors
+    self.embedding = tf.keras.layers.Embedding(self.output_vocab_size,
+                                               embedding_dim)
+
+    # For Step 2. The RNN keeps track of what's been generated so far.
+    self.gru = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+    # For step 3. The RNN output will be the query for the attention layer.
+    self.attention = BahdanauAttention(self.dec_units)
+
+    # For step 4. Eqn. (3): converting `ct` to `at`
+    self.Wc = tf.keras.layers.Dense(dec_units, activation=tf.math.tanh,
+                                    use_bias=False)
+
+    # For step 5. This fully connected layer produces the logits for each
+    # output token.
+    self.fc = tf.keras.layers.Dense(self.output_vocab_size)
+# The `call` method for this layer takes and returns multiple tensors. 
+# Organize those into simple container classes:
